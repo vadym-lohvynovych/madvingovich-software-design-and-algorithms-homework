db8b84e14c72fc62149c3da0677b2caa38618b56
Вадим Логвинович <vadvoker@gmail.com>
2022-07-04 21:15:05 +0300

    create Shape class

--- 2-building-blocks-of-oop-part-1/hometask/src/Shape.ts
+++ 2-building-blocks-of-oop-part-1/hometask/src/Shape.ts
@@ -1,3 +1,39 @@
+import { Point } from "./Point";
+
 export abstract class Shape {
+    protected color: string;
+    protected filled: boolean;
+    protected points: Point[];
+
     abstract getType(): string;
+
+    constructor(points: Point[]);
+    constructor(points: Point[], color?: string, filled?: boolean);
+    constructor(
+        points: Point[],
+        color: string = "green",
+        filled: boolean = true
+    ) {
+        if (points.length < 3) {
+            throw new Error("Figure shoud have at least 3 points.");
+        }
+
+        this.points = points;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    getPerimeter(): number {
+        return this.points.reduce((acc, point, index) => {
+            const nextPointIndex = this.points[index + 1] ? index + 1 : 0;
+            return acc + point.distance(this.points[nextPointIndex]);
+        }, 0);
+    }
+
+    toString() {
+        const filledText = this.filled ? "filled" : "not filled";
+        const pointsText = this.points.map((p) => p.toString()).join(", ");
+
+        return `A Shape with color of ${this.color} and ${filledText}. Points: ${pointsText}.`;
+    }
 }
