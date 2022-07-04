ff28762afc2de59499438f4c7f3cac5da0cc2d4a
Вадим Логвинович <vadvoker@gmail.com>
2022-07-04 20:44:06 +0300

    add point class

--- 2-building-blocks-of-oop-part-1/hometask/src/Point.ts
+++ 2-building-blocks-of-oop-part-1/hometask/src/Point.ts
@@ -1,1 +1,30 @@
-export class Point {}
+export class Point {
+    public x: number;
+    public y: number;
+
+    constructor();
+    constructor(x?: number, y?: number) {
+        this.x = x ?? 0;
+        this.y = y ?? 0;
+    }
+
+    private getDistanceTo(x2: number, y2: number): number {
+        return Math.sqrt(Math.pow(x2 - this.x, 2) + Math.pow(y2 - this.y, 2));
+    }
+
+    distance(): number;
+    distance(x?: Point): number;
+    distance(x?: Point | number, y?: number): number {
+        if (typeof x === "number" && typeof y === "number") {
+            return this.getDistanceTo(x, y);
+        } else if (x instanceof Point) {
+            return this.getDistanceTo(x.x, x.y);
+        } else {
+            return this.getDistanceTo(0, 0);
+        }
+    }
+
+    toString() {
+        return `(${this.x}, ${this.y})`;
+    }
+}
