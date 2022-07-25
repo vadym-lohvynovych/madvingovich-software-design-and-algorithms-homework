5cb153494cecfdefe28d2f4da9eedf8d0860dbbb
Вадим Логвинович <vadvoker@gmail.com>
2022-07-25 18:53:25 +0300

    dataConverter refactoring

--- 4-functional-programming/hometask/src/utils/dataConverter.ts
+++ 4-functional-programming/hometask/src/utils/dataConverter.ts
@@ -4,22 +4,19 @@ import { Row } from "../components/Table";
 const findByUserId = (userId: string) => (entity: Account | Image) => entity.userID === userId;
 const reduceTotalSum = (acc: number, payment: Payment): number => acc + payment.totalSum;
 
-const addAvatar = (images: Image[]) => (user: User) => ({
-  ...user,
-  avatar: images.find(findByUserId(user.userID)).url,
-});
-
-const addPosts = (accounts: Account[]) => (user: User) => ({
-  ...user,
-  posts: accounts.find(findByUserId(user.userID)).posts,
-});
-
-const addLastPayments = (accounts: Account[]) => (user: User) => ({
-  ...user,
-  lastPayments: accounts.find(findByUserId(user.userID)).payments.reduce(reduceTotalSum, 0),
-});
-
 export const dataConverter = (users: User[], accounts: Account[], images: Image[]): Row[] => {
   // @ts-ignore
-  return users.map(addAvatar(images)).map(addPosts(accounts)).map(addLastPayments(accounts));
+  return users.map(({ userID, username, country, name }) => {
+    const { url: avatar } = images.find(findByUserId(userID));
+    const { posts, payments = [] } = accounts.find(findByUserId(userID));
+    return {
+      userID,
+      username,
+      country,
+      name,
+      avatar,
+      posts,
+      lastPayments: payments.reduce(reduceTotalSum, 0),
+    };
+  });
 };
